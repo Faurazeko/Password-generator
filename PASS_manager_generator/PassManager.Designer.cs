38c1650e591372652a9dd850a63da1af9f5b5275
Faurazeko <trozana2@gmail.com>
2020-12-27 20:39:20 +0700

    initial

---
+++ PASS_manager_generator/PassManager.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace PASS_manager_generator
+{
+    partial class PassManager
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.generate = new System.Windows.Forms.Button();
+            this.maintextbox = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // generate
+            // 
+            this.generate.Font = new System.Drawing.Font("Calibri", 50F);
+            this.generate.Location = new System.Drawing.Point(384, 204);
+            this.generate.Name = "generate";
+            this.generate.Size = new System.Drawing.Size(300, 100);
+            this.generate.TabIndex = 0;
+            this.generate.Text = "Generate_Button";
+            this.generate.UseVisualStyleBackColor = true;
+            this.generate.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // maintextbox
+            // 
+            this.maintextbox.Font = new System.Drawing.Font("Calibri", 25F);
+            this.maintextbox.Location = new System.Drawing.Point(12, 310);
+            this.maintextbox.Name = "maintextbox";
+            this.maintextbox.Size = new System.Drawing.Size(1060, 48);
+            this.maintextbox.TabIndex = 3;
+            this.maintextbox.Text = "Result of generation here.";
+            this.maintextbox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.AccessibleName = "";
+            this.numericUpDown1.Font = new System.Drawing.Font("Calibri", 20F);
+            this.numericUpDown1.Location = new System.Drawing.Point(154, 264);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(69, 40);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            32,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 20F);
+            this.label1.Location = new System.Drawing.Point(64, 266);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 33);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "length";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(978, 489);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(94, 13);
+            this.linkLabel1.TabIndex = 7;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "2020 (c) Faurazeko";
+            this.linkLabel1.Click += new System.EventHandler(this.linkLabel1_Click);
+            // 
+            // PassManager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1084, 511);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.maintextbox);
+            this.Controls.Add(this.generate);
+            this.Font = new System.Drawing.Font("Calibri", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "PassManager";
+            this.ShowIcon = false;
+            this.Text = "Faurazeko`s password generator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        protected System.Windows.Forms.Button generate;
+        private System.Windows.Forms.TextBox maintextbox;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ PASS_manager_generator/PassManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PASS_manager_generator
+{
+    public partial class PassManager : Form
+    {
+        int length = 32;
+        public PassManager()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            var pass = new StringBuilder();
+
+            for (int i = 0; i < length; i++)
+            {
+                pass.Append((char)rnd.Next(33, 126));
+            }
+            maintextbox.Text = pass.ToString();
+            Clipboard.SetText(maintextbox.Text);
+        }
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            length = (int)numericUpDown1.Value;
+        }
+
+        private void linkLabel1_Click(object sender, EventArgs e)
+        {
+            maintextbox.Text = "https://github.com/Faurazeko";
+            Clipboard.SetText(maintextbox.Text);
+        }
+    }
+}
